b29d995ad39d7bfd3e31825ca0476116194ce0e3
hiejulia <hienminhnguyen711@gmail.com>
2020-10-10 19:09:17 +0300

    add exer

---
+++ master-computer-science-mit/15.075J-Statistical-Thinking-And-Data-Analysis/assignments/Exe3-HienNguyen.m
@@ -0,0 +1,24 @@
+﻿# 15.075StatisticalThinkingandDataAnalysis 3
+
+# E1 
+
+clear all;
+clc;
+
+alpha = 0.1;
+
+x = [488 478 480 426 440 410 458 460];
+y = [484 478 492 444 436 398 464 476];
+
+[h,p,ci,stats] = ttest(x,y,alpha,'left')
+
+# The upper 90% CI is(‐∞,1.3746], which includes 0,so we do not reject H0
+
+
+
+
+# E 2
+
+
+
+
